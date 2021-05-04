e28a8e4db7dd8d9392a5b418fa9c89c44e38eea8
Vasil Kostadinov <1zmgaming1@gmail.com>
2021-05-04 20:42:04 +0300

    0.2.1.1 patch

--- src/PubNubChat/components/Pubnub.js
+++ src/PubNubChat/components/Pubnub.js
@@ -1,9 +1,6 @@
 import PubNub from 'pubnub'
 
 export const pubnub = new PubNub({
-    // publishKey: 'pub-c-090e14f1-3e54-4e03-bef8-ad58c58476e9',
-    // subscribeKey: 'sub-c-b6fbfee2-ab7d-11eb-8f4a-b6e2b128ec2a',
     publishKey: process.env.REACT_APP_PUBLISH_KEY,
     subscribeKey: process.env.REACT_APP_SUBSCRIBE_KEY,
-    // uuid: 'USERNAME',
 })
